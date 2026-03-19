16bf91677f91262d88c0d11eda78e1ef3955cefc
M Juma <qasimimohammad819@gmail.com>
2026-03-19 17:53:30 +0100

    create place model and placeImage model

--- place/models.py
+++ place/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
+from city.models import City
+from category.models import Category
+
 # Create your models here.
+
+class Place(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.TextField( blank=True,null=False)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True,blank=True)
+    city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True,blank=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,blank=True)
+    
+
+    def __str__(self):
+        return self.name
+
+
+# place images model
+class PlaceImage(models.Model):
+    place = models.ForeignKey(Place,on_delete=models.CASCADE,related_name='images')
+    image = models.ImageField(upload_to='places/') # '/media/place/' (save)
+
+    def __str__(self):
+        return f'Image for {self.place.name}'

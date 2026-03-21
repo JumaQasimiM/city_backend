25177e5bb9a9f7a1ced6b3466936f745382d6c3a
M Juma <qasimimohammad819@gmail.com>
2026-03-21 22:11:15 +0100

    update place model

--- place/models.py
+++ place/models.py
@@ -7,15 +7,32 @@ from category.models import Category
 
 
 # Create your models here.
+# services 
+class Service(models.Model):
+    title = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.title
+    
 class Place(models.Model):
     name = models.CharField(max_length=40)
     description = models.TextField( blank=True,null=False)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True,blank=True)
-    city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True,blank=True)
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True,blank=True,related_name='places')
+    city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True,blank=True,related_name='places')
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,blank=True,related_name='places')
+    services = models.ManyToManyField(Service,related_name='places')
+    register_date = models.DateField(auto_now=True)
     
-
+ # model configuration (database + admin display)
+    class Meta:
+        db_table = 'places_table'  # custom table name in database
+        verbose_name = 'Place'  # singular name in admin panel
+        verbose_name_plural = 'places' # plural name in admin panel
+        ordering = ['city','-register_date']
+        indexes = [
+            models.Index(fields=['name','category'])
+        ]
+  
     def __str__(self):
         return self.name
 

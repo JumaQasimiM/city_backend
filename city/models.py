ed7d83b9f8c4cf8c56b6d4684bd811550492593c
M Juma <qasimimohammad819@gmail.com>
2026-03-21 22:10:20 +0100

    create city model

--- city/models.py
+++ city/models.py
@@ -8,6 +8,12 @@ class City(models.Model):
     description = models.TextField()
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
 
+ # model configuration (database + admin display)
+    class Meta:
+        db_table = 'cities_table'  # custom table name in database
+        verbose_name = 'City'  # singular name in admin panel
+        verbose_name_plural = 'cities' # plural name in admin panel
+  
     def __str__(self):
         return self.name
 
@@ -17,5 +23,9 @@ class CityImage(models.Model):
     city = models.ForeignKey(City, on_delete=models.CASCADE,related_name='city_image')
     image = models.ImageField(upload_to='cities')
 
+ # model configuration (database + admin display)
+    class Meta:
+        db_table = 'cities_image_table'  # custom table name in database
+        
     def __str__(self):
         return f"Image for - {self.city.name}"

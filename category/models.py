7095dbda9d88813089c6735b3e0f9921810290ae
M Juma <qasimimohammad819@gmail.com>
2026-04-06 22:26:35 +0200

    update category model

--- category/models.py
+++ category/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.functions import Lower
 
 class Category(models.Model):
     title = models.CharField(max_length=50)
@@ -8,8 +9,16 @@ class Category(models.Model):
         db_table = 'categories_table'  # custom table name in database
         verbose_name = 'Category'  # singular name in admin panel
         verbose_name_plural = 'Categories'  # plural name in admin panel
+
+        constraints = [ # title must be unique 
+            models.UniqueConstraint(
+                Lower('title'),
+                name='unique_lower_title'
+            )
+        ]
     
     
     def __str__(self):
         return self.title
     
+

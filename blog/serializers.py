036bcfe50c79be92172160c28e84fead06b7dd78
M Juma <qasimimohammad819@gmail.com>
2026-04-06 18:46:06 +0200

    add serializer for Blog

---
+++ blog/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import Blog, BlogComment
+from accounts.serializers import UserSerializer
+
+class BlogCommentSerializer(serializers.ModelSerializer):
+    """
+    read_only: fields are returned in the API response,
+    but cannot be set or modified by the user.
+    """
+    user = UserSerializer(read_only=True)
+    blog = serializers.PrimaryKeyRelatedField(read_only=True) 
+
+    class Meta:
+        model = BlogComment
+        fields = '__all__'
+
+
+class BlogSerializer(serializers.ModelSerializer):
+    comments = BlogCommentSerializer(many=True, read_only=True)  # related_name='comments' -- in model
+
+    class Meta:
+        model = Blog
+        fields = '__all__'

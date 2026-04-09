e2f81fbae77c3495e82c82e0dc89650060c93394
M Juma <qasimimohammad819@gmail.com>
2026-04-09 13:18:36 +0200

    update category serializer add nested serializer

--- category/serializers.py
+++ category/serializers.py
@@ -3,6 +3,22 @@ from .models import Category
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    # فقط نام یا id مکان‌ها را نشان می‌دهد
+    # places = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    # or
+    # places = serializers.StringRelatedField(many=True)
+    place_name = serializers.SerializerMethodField(method_name= 'get_place_name')
+
+    # calculates and returns the count of related Place objects for each Category
+    totle_place_per_category = serializers.SerializerMethodField(method_name='get_total_place_per_category')
+
     class Meta:
         model = Category
-        fields = '__all__'
+        fields = ['id', 'title','place_name','totle_place_per_category']
+    
+    def get_place_name(self,obj): # ojb = Place
+        return [place.name for place in obj.places.all()]
+    
+    #
+    def get_total_place_per_category(self,obj):
+        return obj.places.count()

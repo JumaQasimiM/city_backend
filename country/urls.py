99c0b235a87f70021ce48367bfe93039efe53ef3
M Juma <qasimimohammad819@gmail.com>
2026-04-12 21:50:19 +0200

    feat(api): implement country endpints
    - add GET/POST for country list
    - add PATCH/DELETE for country detail
    - improve validation and error handling

--- country/urls.py
+++ country/urls.py
@@ -2,7 +2,10 @@
 from django.urls import path
 from . import views
 
+'''
+REST : METHOD 
+'''
 urlpatterns = [
-    path('',views.county_list),
-    path('create/',views.create_country),
+    path('',views.create_list_country),
+    path('<int:pk>/',views.country_detail),
 ]
--- country/views.py
+++ country/views.py
@@ -6,33 +6,53 @@ from .models import Country
 from.serializers import CountrySerializer
 
 
-# list countries
-@api_view(['GET'])
-def county_list(request):
-    countries = Country.objects.all()
-    serializer = CountrySerializer(countries,many =True)
-    return Response(serializer.data)
-    
 
-# create new country
-@api_view(['POST'])
-def create_country(request):
-    serializer = CountrySerializer(data = request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response({'success':'country created successfully.'},status=status.HTTP_200_OK)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# update the country
-@api_view(['PATCH'])
-def update_country(request,pk):
-    pass
-# delete the country
-@api_view(['DELETE'])
-def delete_country(request,pk):
-    pass
-# country detail
-@api_view(['GET'])
+
+
+# REST method
+
+'''
+GET: list all countriew
+POST: create new country
+'''
+@api_view(['GET','POST'])
+def create_list_country(request):
+    if request.method =='GET':
+        countries = Country.objects.all()
+        serializer = CountrySerializer(countries,many =True)
+        return Response(serializer.data)
+    elif request.method =='POST':
+        serializer = CountrySerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'success':'country created successfully.'},status=status.HTTP_201_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+@api_view(['GET','DELETE','PATCH'])
 def country_detail(request,pk):
-    pass
+    if request.method == 'GET':
+        country = get_object_or_404(Country, pk = pk)
+        serializer = CountrySerializer(country)
+        return Response(serializer.data,status=status.HTTP_200_OK)
 
+    elif request.method == 'DELETE':
+        country = get_object_or_404(Country,pk = pk)
+        if country.cities.exists():
+            return Response(
+                {'error':'Can not delete country with existing city.'},
+                status=status.HTTP_204_BAD_REQUEST
+            )
+        country.delete()
+        return Response( {'success': 'country removed successfully'},status=status.HTTP_200_OK)   
+
+    elif request.method == 'PATCH':
+        country = get_object_or_404(Country,pk=pk)
+        serializer = CountrySerializer(country, data = request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'success':'Country updated successfully!','data':serializer.data},status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

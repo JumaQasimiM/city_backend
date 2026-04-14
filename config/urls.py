04c90f7b988ac6014db255ec8880fee6f166763b
M Juma <qasimimohammad819@gmail.com>
2026-04-14 16:28:05 +0200

    feat:register city urls in config urls

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/categories/', include('category.urls')),
     path('api/countries/', include('country.urls')),
+    path('api/cities/', include('city.urls')),
 ]

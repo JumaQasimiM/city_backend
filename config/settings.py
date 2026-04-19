22daa1ed5315e8eaf334d761ac2a3467c3467fec
M Juma <qasimimohammad819@gmail.com>
2026-04-19 18:17:37 +0200

    update .... serializer,views,urls

--- config/settings.py
+++ config/settings.py
@@ -132,11 +132,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+
 STATIC_URL = 'static/'
 
-STATICFILES = [
+STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- place/views.py
+++ place/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from . models import Place,PlaceComment,PlaceImage,PlaceLike,FavoratePlace
+from serializers import PlaceSerializer,PlaceImageSerializer,PlaceLikeSerializer,PlaceCommentSerializer,FavoritePlaceSerializer
+
+
+
+
+
+# create 
+# list
+# update
+# delete

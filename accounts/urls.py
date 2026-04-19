3270b4572f6fb387a445fed62647be2f33ef0a45
M Juma <qasimimohammad819@gmail.com>
2026-04-19 18:17:03 +0200

    feat:create urls for register and userlist

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .models import User
+from . import views
 
-urlspatterns =[
-    # path('',)
+urlpatterns  =[
+    path('',views.users),
+    path('register/',views.register_user),
+    path('<int:pk>/',views.user_detail)
 ]

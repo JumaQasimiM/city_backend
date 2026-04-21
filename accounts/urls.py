97fa445ca6017970bee8f4a5840719f0c3c1e096
M Juma <qasimimohammad819@gmail.com>
2026-04-21 09:48:42 +0200

    feat: create url for login and register user in user/urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns  =[
     path('',views.users),
     path('register/',views.register_user),
-    path('<int:pk>/',views.user_detail)
+    path('<int:pk>/',views.user_detail),
+    # login
+    path('login/', views.LoginView.as_view()),
+    path('refresh/', TokenRefreshView.as_view()),
 ]

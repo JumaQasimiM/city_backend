7860c0aac3925ed717263e7b1d5014532764b21d
M Juma <qasimimohammad819@gmail.com>
2026-04-23 11:00:20 +0200

    create blog view with viewset

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# Create your views here.
-    # muss in der view schreiben.
-def perform_create(self, serializer):
-    serializer.save(user=self.request.user)
-    
+# # Create your views here.
+#     # muss in der view schreiben.
+# def perform_create(self, serializer):
+#     serializer.save(user=self.request.user)
+
+
+from rest_framework.viewsets import ModelViewSet
+
+from . import models
+from . import serializers
+
+class BlogViewSet(ModelViewSet):
+    queryset = models.Blog.objects.all()
+    serializer_class = serializers.BlogSerializer

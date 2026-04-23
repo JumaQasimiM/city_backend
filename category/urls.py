18624f04c2b463026964dc5f15c7f26b8e9e709b
M Juma <qasimimohammad819@gmail.com>
2026-04-23 11:11:58 +0200

    fix: update category url with router

--- category/urls.py
+++ category/urls.py
@@ -1,11 +1,19 @@
-from django.urls import path
-from . import views
+# from django.urls import path
+# from . import views
 
-urlpatterns =[
-    path('',views.categories,name='category_list'),
-    path('create/',views.create_category),
-    path('category_detail/<int:pk>/',views.category_detail),
-    path('delete_category/<int:pk>/',views.delete_category),
-    path('update_category/<int:pk>/',views.update_category)
+# urlpatterns =[
+#     path('',views.categories,name='category_list'),
+#     path('create/',views.create_category),
+#     path('category_detail/<int:pk>/',views.category_detail),
+#     path('delete_category/<int:pk>/',views.delete_category),
+#     path('update_category/<int:pk>/',views.update_category)
     
-]
+# ]
+
+from . import views 
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+router.register('',views.CategoryViewSet)
+urlpatterns  = router.urls

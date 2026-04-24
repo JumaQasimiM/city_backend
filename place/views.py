5a31a27ac2af4015f44a3d08aad72e6d3a782302
M Juma <qasimimohammad819@gmail.com>
2026-04-24 21:41:46 +0200

    update place-view.py

--- place/views.py
+++ place/views.py
@@ -1,12 +1,16 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.parsers import MultiPartParser,FormParser
 
-from . models import Place,PlaceComment,PlaceImage,PlaceLike,FavoratePlace
-from . serializers import PlaceSerializer,PlaceImageSerializer,PlaceLikeSerializer,PlaceCommentSerializer,FavoritePlaceSerializer
+from . models import Place,PlaceComment,PlaceImage,PlaceLike,FavoratePlace,Service
+from . serializers import PlaceSerializer,PlaceImageSerializer,ServiceSerializer,PlaceLikeSerializer,PlaceCommentSerializer,FavoritePlaceSerializer
 
 
 
+# services
 
+class ServiceViewSet(ModelViewSet):
+   queryset = Service.objects.all()
+   serializer_class = ServiceSerializer
 
 class PlaceViewSet(ModelViewSet):
    queryset = Place.objects.all()

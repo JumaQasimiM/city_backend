32af12c10a9b23bab3659774075fde42e27d5f5c
M Juma <qasimimohammad819@gmail.com>
2026-04-26 09:07:02 +0200

    create filter in placeviewSet for search a place with city or category using django-filter

--- place/views.py
+++ place/views.py
@@ -4,6 +4,8 @@ from rest_framework.parsers import MultiPartParser,FormParser
 from . models import Place,PlaceComment,PlaceImage,PlaceLike,FavoratePlace,Service
 from . serializers import PlaceSerializer,PlaceImageSerializer,ServiceSerializer,PlaceLikeSerializer,PlaceCommentSerializer,FavoritePlaceSerializer
 
+# filter and search 
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 # services
@@ -15,6 +17,30 @@ class ServiceViewSet(ModelViewSet):
 class PlaceViewSet(ModelViewSet):
    queryset = Place.objects.all()
    serializer_class = PlaceSerializer
+
+   # filter and search with django filter - library
+   filter_backends = [DjangoFilterBackend]
+   filterset_fields = ['city', 'category']
+
+   '''
+   /places?city=1
+   /places?category=2
+   /places?city=1&category=2
+   '''
+
+   # def get_queryset(self):
+   #      queryset = Place.objects.all()
+
+   #      city = self.request.query_params.get('city')
+   #      category = self.request.query_params.get('category')
+
+   #      if city:
+   #          queryset = queryset.filter(city_id=city)
+
+   #      if category:
+   #          queryset = queryset.filter(category_id=category)
+
+   #      return queryset
 
 class PlaceImageViewSet(ModelViewSet):
    queryset = PlaceImage.objects.all()

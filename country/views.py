c997e1d8f5a11eb6ad73f9b3eb19420fe11c17e7
M Juma <qasimimohammad819@gmail.com>
2026-05-01 17:17:34 +0200

    update and apply permission

--- country/views.py
+++ country/views.py
@@ -1,16 +1,13 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 
 from .models import Country
 from .serializers import CountrySerializer
+from helper.permission import IsAdminOrReadOnly
 
 """
-Country API (DRF - function based views)
-
-This API provides basic CRUD operations for Country model.
-
 Endpoints:
 --------------------------------------------------
 GET     /countries/          -> List all countries
@@ -26,11 +23,9 @@ DELETE  /countries/<id>/     -> Delete a country (if no related cities)
 # ==============================
 # LIST & CREATE
 # ==============================
-"""
-GET  : return all countries
-POST : create a new country
-"""
+
 @api_view(['GET', 'POST'])
+@permission_classes([IsAdminOrReadOnly])
 def create_list_country(request):
 
     # ---------- GET ----------
@@ -40,15 +35,7 @@ def create_list_country(request):
         return Response(serializer.data)
 
     # ---------- POST ----------
-    elif request.method == 'POST':
-        #  check login
-        if not request.user.is_authenticated:
-            return Response({"message": "Login required"}, status=401)
-
-        #  only admin
-        if request.user.role != "admin":
-            return Response({"message": "Permission denied"}, status=403)
-
+    elif request.method == 'POST':   
         serializer = CountrySerializer(data=request.data)
 
         if serializer.is_valid():
@@ -57,17 +44,15 @@ def create_list_country(request):
                 {"message": "Country created", "data": serializer.data},
                 status=201
             )
-
         return Response(serializer.errors, status=400)
+    
+
 # ==============================
 # DETAIL / UPDATE / DELETE
 # ==============================
-"""
-GET    : retrieve a single country
-PATCH  : update country partially
-DELETE : delete country (only if no related cities)
-"""
+
 @api_view(['GET', 'PATCH', 'DELETE'])
+@permission_classes([IsAdminOrReadOnly])
 def country_detail(request, pk):
 
     country = get_object_or_404(Country, pk=pk)
@@ -76,36 +61,22 @@ def country_detail(request, pk):
     if request.method == 'GET':
         serializer = CountrySerializer(country)
         return Response(serializer.data)
-
-    #  check login for protected actions
-    if not request.user.is_authenticated:
-        return Response({"message": "Login required"}, status=401)
-
-    #  admin
-    if request.user.role != "admin":
-        return Response({"message": "Permission denied"}, status=403)
-
+    
     # ---------- DELETE ----------
     if request.method == 'DELETE':
         if country.cities.exists():
-            return Response(
-                {"message": "Cannot delete country with cities"},
-                status=400
-            )
-
+            return Response({"message": "Cannot delete country with cities"},status=400)
         country.delete()
-        return Response({"message": "Deleted"}, status=204)
+        return Response(status=204)
 
     # ---------- PATCH ----------
     if request.method == 'PATCH':
         serializer = CountrySerializer(country, data=request.data, partial=True)
-
         if serializer.is_valid():
             serializer.save()
             return Response(
                 {"message": "Updated", "data": serializer.data}
             )
-
         return Response(serializer.errors, status=400)
 
     # get object or return 404
@@ -130,10 +101,6 @@ def country_detail(request, pk):
 
         country.delete()
         return Response(
-            {
-                'success': True,
-                'message': 'Country removed successfully.'
-            },
             status=status.HTTP_204_NO_CONTENT
         )
 

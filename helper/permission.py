f8ea9daf648b96008c816a181910a4e6ec9b4f35
M Juma <qasimimohammad819@gmail.com>
2026-05-01 17:15:46 +0200

    update permission, and apply the permission base on admin and viewer

--- helper/permission.py
+++ helper/permission.py
@@ -1,8 +1,9 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsAdminOrReadOnly(BasePermission):
+    message = "You do not have permission to perform this action."
     def has_permission(self, request, view):
-        # GET, HEAD, OPTIONS → all have permision 
+        # GET, HEAD, OPTIONS → all have permision --> this is for viewer user
         if request.method in SAFE_METHODS:
             return True
 
@@ -12,3 +13,18 @@ class IsAdminOrReadOnly(BasePermission):
             request.user.is_authenticated and
             request.user.role == "admin"
         )
+
+    
+class IsAdminBusinessOrReadOnly(BasePermission):
+
+    def has_permission(self, request, view):
+
+        # GET برای همه (حتی بدون login)
+        if request.method in SAFE_METHODS:
+            return True
+
+        # بقیه فقط با login
+        if not request.user or not request.user.is_authenticated:
+            return False
+
+        return request.user.role in ['admin', 'business']

8db245b0d8146b86e832d4d34f988ee4ad42f951
M Juma <qasimimohammad819@gmail.com>
2026-05-03 11:28:09 +0200

    remove pycache files

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Exit on error
+set -o errexit
+
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "Running migrations..."
+python manage.py migrate
+
+echo "Build finished successfully!"
